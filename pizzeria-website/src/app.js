0fdd124a01cea464329bbb5fc9bc01a24850a620
Arnaud Lemarquand <arnaud.lemarquand@gmail.com>
2017-01-27 17:12:24 +0100

    implement navbar

--- pizzeria-website/src/app.js
+++ pizzeria-website/src/app.js
@@ -1,10 +1,17 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
-import home from './home'
 
+import HomeModule from './home'
 
-angular.module('pizzeria', [ngRoute, home])
-    .config(function ($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider.otherwise('/')
-    });
+angular.module('pizzeria', [
+    ngRoute,
+    HomeModule
+])
+.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true); 
+    $routeProvider
+        .when('/', {
+            template: '<home></home>'
+        })
+        .otherwise('/')
+});
