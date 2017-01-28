300f1dcfb4d06bcd88d04d6bf54cb8c331774d33
joelTo <joel.torib@gmail.com>
2017-01-28 15:45:16 +0100

    Rajout Ajout des tables {entree, ingredient, boisson} plus leurs dépendances.

--- proposition_sql_modification .sql
+++ proposition_sql_modification .sql
@@ -52,35 +52,22 @@ CREATE TABLE administrateur (
     code_pizza varchar(15) NOT NULL,
     nom varchar(5000) NOT NULL,
     url  varchar(5000) NOT NULL,
-    prix  decimal (5000) NOT NULL,
+    prix  decimal NOT NULL,
     note int  NOT NULL,
   	categorie varchar(5000) NOT NULL
    );
 
-CREATE TABLE pizza (
-    id int PRIMARY KEY, 
-    code_pizza varchar(15) NOT NULL,
-    nom varchar(5000) NOT NULL,
-    url_pizza  varchar(5000) NOT NULL,
-    prix  decimal (5000) NOT NULL,
-    note int  NOT NULL,
-  	categorie varchar(5000) NOT NULL
-   );
 
 CREATE TABLE pizza_commande(
     id int primary key,
     id_pizza int not null,
-    foreign key (id_pizza) REFERENCES pizza(id),
-    id_commande int not null --,
-    foreign key (id_commande) REFERENCES commande(id)
+    id_commande int not null    
 );
 
 CREATE TABLE menu_commande(
     id int primary key,
     id_pizza int not null,
-    foreign key (id_pizza) REFERENCES pizza(id),
-    id_commande int not null,
-    foreign key (id_commande) REFERENCES commande(id)
+    id_commande int not null
 );
 
 CREATE TABLE promotion (
@@ -89,8 +76,8 @@ CREATE TABLE promotion (
     nom varchar(5000) NOT NULL,
     description_promotion varchar(5000) NOT NULL,
     reduction  decimal, 
-    type_reduction int,
-   )
+    type_reduction int
+);
 
 CREATE TABLE commande (
     id int PRIMARY KEY, 
@@ -101,7 +88,19 @@ CREATE TABLE commande (
     code_promotion int not null, 
     prix decimal not null, 
     id_menu_commande int not null
-   )
+);
+
+
+ALTER TABLE pizza_commande add constraint foreign key (id_pizza) REFERENCES pizza(id);
+ALTER TABLE pizza_commande add constraint foreign key (id_commande) REFERENCES commande(id);
+
+
+ALTER TABLE menu_commande add constraint foreign key (id_pizza) REFERENCES pizza(id);
+ALTER TABLE menu_commande add constraint foreign key (id_commande) REFERENCES commande(id);
+   
+
+   
+   
 
 ALTER TABLE commande add constraint foreign key (id_livreur) REFERENCES livreur(id);
 ALTER TABLE commande add constraint foreign key (id_client) REFERENCES client(id);
@@ -111,7 +110,38 @@ ALTER TABLE commande add constraint foreign key (id_menu_commande) REFERENCES me
 
 CREATE TABLE dessert(
     id int PRIMARY KEY, 
-    nom varchar(5000) not null, 
+    nom varchar(5000) not null,
     prix decimal not null,
-    url_dessert varchar(5000)
+    url_dessert varchar(5000) NOT NULL
+    );
+
+-- Ajout des tables {entree, ingredient, boisson} plus leurs dépendances.
+
+CREATE TABLE entree(
+    id int PRIMARY KEY, 
+    nom varchar(5000) not null,
+    prix decimal not null,
+    url_entree varchar(5000)
+    );
+
+CREATE TABLE ingredient(
+    id int PRIMARY KEY, 
+    nom varchar(5000) not null 
+    );
+
+  
+  ALTER TABLE pizza ADD id_ingredient_pizza int not null; 
+  ALTER TABLE pizza add constraint foreign key (id_ingredient_pizza) REFERENCES ingredient(id);
+
+  ALTER TABLE entree ADD id_ingredient_entree int not null;
+  ALTER TABLE entree add constraint foreign key (id_ingredient_entree) REFERENCES ingredient(id);
+
+  ALTER TABLE dessert ADD id_ingredient_dessert int not null;
+  ALTER TABLE dessert add constraint foreign key (id_ingredient_dessert) REFERENCES ingredient(id);
+
+  CREATE TABLE boisson(
+    id int PRIMARY KEY, 
+    nom varchar(5000) not null,
+    prix decimal not null,
+    url_boisson varchar(5000)
     );
