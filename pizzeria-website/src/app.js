2a68fab6be14973be479cedc42b5c86a4303cda1
Arnaud Lemarquand <arnaud.lemarquand@gmail.com>
2017-01-30 10:40:24 +0100

    mise à jour architecture projet / componenet defini dans app.js

--- pizzeria-website/src/app.js
+++ pizzeria-website/src/app.js
@@ -2,43 +2,47 @@ import angular from 'angular'
 import ngRoute from 'angular-route'
 import ngStorage from 'ngstorage';
 
-import home from './home'
-
+import { Home } from './home/home.component'
+import { Product } from './product/product.component'
+import { ModalPizza } from './modal/modal.component'
 
 angular.module('pizzeria', [
     ngRoute,
-    'ngStorage',
-    home
+    'ngStorage'
 ])
 
-    .controller('lsCtrl', function (
-        $scope,
-        $localStorage,
-        $sessionStorage
-    ) {
+.component('home', Home)
+.component('product', Product)
+.component('modalPizza', ModalPizza)
 
-        $scope.$localStorage = $localStorage;
-        $scope.$sessionStorage = $sessionStorage;
+.controller('lsCtrl', function (
+    $scope,
+    $localStorage,
+    $sessionStorage
+) {
 
-        if ($localStorage.jsonPanier === undefined) {
-            $localStorage.jsonPanier = {};
-            $localStorage.jsonPanier['pizza'] = [];
-            $localStorage.jsonPanier['boisson'] = [];
-            $localStorage.jsonPanier['dessert'] = [];
-            $localStorage.jsonPanier['menus'] = [];
-        }
+    $scope.$localStorage = $localStorage;
+    $scope.$sessionStorage = $sessionStorage;
 
-    })
+    if ($localStorage.jsonPanier === undefined) {
+        $localStorage.jsonPanier = {};
+        $localStorage.jsonPanier['pizza'] = [];
+        $localStorage.jsonPanier['boisson'] = [];
+        $localStorage.jsonPanier['dessert'] = [];
+        $localStorage.jsonPanier['menus'] = [];
+    }
 
-    .config(function ($routeProvider, $locationProvider) {
+})
 
-        $locationProvider.html5Mode(true);
+.config(function ($routeProvider, $locationProvider) {
 
-        $routeProvider
+    $locationProvider.html5Mode(true);
 
-            .when('/', {
-                template: `<home><home>`
-            })
+    $routeProvider
 
-            .otherwise('/')
-    });
+        .when('/', {
+            template: `<home><home>`
+        })
+
+        .otherwise('/')
+});
