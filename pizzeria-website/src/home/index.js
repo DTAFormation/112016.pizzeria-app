91dad93ae453adcf3d2835b7c0b0bca1c35bd420
Arnaud Lemarquand <arnaud.lemarquand@gmail.com>
2017-01-30 10:28:58 +0100

    USW008 - Visualiser details #25 Implementation de la modal avec component

--- pizzeria-website/src/home/index.js
+++ pizzeria-website/src/home/index.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 
 import { Home } from './home.component'
 import { Product } from '../product/product.component'
+import { ModalPizza } from '../modal/modal.component'
 
 //import HomeController from './home.controller';
 
@@ -9,5 +10,6 @@ export default angular.module('PizzeriaHome', [])
 
 .component('home', Home)
 .component('product', Product)
+.component('modalPizza', ModalPizza)
 
 .name;
---
+++ pizzeria-website/src/modal/modal.component.js
@@ -0,0 +1,12 @@
+class ModalController {
+    constructor() {
+
+    }
+}
+
+export const ModalPizza = {
+    bindings: {
+        pizza: '<'
+    },
+    template: require('./modal.component.html')
+}
