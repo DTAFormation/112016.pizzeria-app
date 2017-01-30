8f17f7206d79dd786c84874adce90f1aaaa066ba
Livio Asdrubal <asdrubal.livio@gmail.com>
2017-01-30 16:55:31 +0100

    USW007 mon compte #24

--- pizzeria-website/src/app.js
+++ pizzeria-website/src/app.js
@@ -1,13 +1,15 @@
-import angular from 'angular'
-import ngRoute from 'angular-route'
+import angular from 'angular';
+import ngRoute from 'angular-route';
 import ngStorage from 'ngstorage';
 
-import { Home } from './home/home.component'
-import { Product } from './product/product.component'
-import { ModalPizza } from './modal/modal.component'
-
-import { PizzaService } from './shared/service/pizza.service'
+import { Home } from './home/home.component';
+import { Product } from './product/product.component';
+import { ModalPizza } from './modal/modal.component';
 import { Panier } from './panier/panier.component';
+import { MonCompte } from './moncompte/moncompte.component';
+
+import { PizzaService } from './shared/service/pizza.service';
+import { UserService } from './shared/service/users.service';
 
 angular.module('pizzeria', [
         ngRoute,
@@ -17,7 +19,9 @@ angular.module('pizzeria', [
     .component('product', Product)
     .component('modalPizza', ModalPizza)
     .component('panier', Panier)
+    .component('moncompte', MonCompte)
     .service('PizzaService', PizzaService)
+    .service('UserService', UserService)
     .config(function ($routeProvider, $locationProvider) {
 
         $locationProvider.html5Mode(true);
@@ -28,6 +32,9 @@ angular.module('pizzeria', [
             })
             .when('/panier', {
                 template: `<panier></panier>`
+            })
+            .when('/moncompte/:id?',{
+                template: `<moncompte></moncompte>`
             })
             .otherwise('/')
     });
