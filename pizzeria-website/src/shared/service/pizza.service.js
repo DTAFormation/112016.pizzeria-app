a4a1191743420b87c7e7236ffd2b9d179a471486
Livio Asdrubal <asdrubal.livio@gmail.com>
2017-01-30 18:52:31 +0100

    USW007 + mon compte #24 (En cours)

--- pizzeria-website/src/shared/service/pizza.service.js
+++ pizzeria-website/src/shared/service/pizza.service.js
@@ -12,18 +12,19 @@ export class PizzaService {
         const pizzas = [{
             id: 0,
             type: 'pizza',
-            name: "Margherita",
-            src: "http://mister-check.e-monsite.com/medias/images/pizza2.jpg"
+            nom: "Margherita",
+            urlImage: "http://mister-check.e-monsite.com/medias/images/pizza2.jpg"
         }, {
             id: 1,
             type: 'pizza',
-            name: "Peperoni",
-            src: "http://timmatic.com/i/2016/12/pepperoni-pizza-wallpaper-wide.jpg"
+            nom: "Peperoni",
+            urlImage: "http://timmatic.com/i/2016/12/pepperoni-pizza-wallpaper-wide.jpg"
         }, {
             id: 2,
             type: 'pizza',
-            name: "Reine",
-            src: "http://astucelle.com/wp-content/uploads/2016/11/image-41.jpeg"
+            nom: "Reine",
+            urlImage: "http://astucelle.com/wp-content/uploads/2016/11/image-41.jpeg"
+
         }];
 
         return this.$q.resolve(pizzas);
