1b43dbb45290401f276887c4fe094f360ed6b2f2
litlleL@litlleL-Pc <litlleL@litlleL-Pc>
2017-01-30 20:23:39 +0100

    USW007 + mon compte #24 Modification des infos cliente

--- pizzeria-website/src/moncompte/moncompte.controller.js
+++ pizzeria-website/src/moncompte/moncompte.controller.js
@@ -1,38 +1,40 @@
 export default class MonCompteController {
 
-    constructor(UserService,$routeParams, $location){
+    constructor(UserService, $routeParams, $location) {
 
         this.UserService = UserService;
         this.id = $routeParams.id;
         this.$location = $location;
 
+        this.UserService.getUser(this.id)
+            .then(user => this.user = user[0]);
+
     }
 
-    $onInit(){
-
-        this.UserService.getUser(this.id)
-            .then(user => this.user = user);
+    $onInit() {
 
         this.disable = true;
 
     }
 
-    updateInfo(){
+    updateInfo() {
 
         this.disable = false;
 
     }
 
-    saveUser(form, user){
-           if (form.$invalid) return;
-           this.UserService.saveUser(user)
-                .then(() => this.$location.path('/'));
+    saveUser(form, user) {
+        if (form.$invalid) return;
+        this.UserService.saveUser(user)
+            .then(() => this.$location.path('/moncompte/' + user.id));
+
+        this.disable = true;
     }
 
-    annulerUpdate(){
-        
-         this.disable = true;
-         this.UserService.getUser(this.id)
+    annulerUpdate() {
+
+        this.disable = true;
+        this.UserService.getUser(this.id)
             .then(user => this.user = user);
 
     }
