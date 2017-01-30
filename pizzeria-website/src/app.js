585580c3dee934e95671194f4780427cfde7d1f0
Livio Asdrubal <asdrubal.livio@gmail.com>
2017-01-30 16:55:31 +0100

    USW007 mon compte #24

--- pizzeria-website/src/app.js
+++ pizzeria-website/src/app.js
@@ -1,20 +1,19 @@
-import angular from 'angular'
-import ngRoute from 'angular-route'
+import angular from 'angular';
+import ngRoute from 'angular-route';
 import ngStorage from 'ngstorage';
 
-import { Home } from './home/home.component'
-import { Product } from './product/product.component'
-import { ModalPizza } from './modal/modal.component'
-
-import { Inscription } from './inscription/inscription.component'
-import { UserService } from './shared/service/user.service'
-import { Login } from './login/login.component'
-import { PizzaService } from './shared/service/pizza.service'
-import { PanierService } from './shared/service/panier.service'
+import { Home } from './home/home.component';
+import { Product } from './product/product.component';
+import { ModalPizza } from './modal/modal.component';
+import { MonCompte } from './moncompte/moncompte.component';
 import { Panier } from './panier/panier.component';
 import { Pizza } from './pizza/pizza.component';
+import { Login } from './login/login.component';
+import { Inscription } from './inscription/inscription.component';
 
-
+import { UserService } from './shared/service/user.service';
+import { PizzaService } from './shared/service/pizza.service';
+import { PanierService } from './shared/service/panier.service';
 
 
 
@@ -28,10 +27,15 @@ angular.module('pizzeria', [
     .component('panier', Panier)
     .component('login', Login)
     .component('pizza', Pizza)
+    .component('inscription',Inscription)
+    .component('moncompte', MonCompte)
+
+    .service('PanierService', PanierService)
     .service('PizzaService', PizzaService)
     .service('UserService', UserService)
-    .component('inscription',Inscription)
-    .service('PanierService', PanierService)
+    .service('PizzaService', PizzaService)
+    .service('UserService', UserService)
+
     .config(function ($routeProvider, $locationProvider) {
 
         $locationProvider.html5Mode(true);
@@ -43,18 +47,17 @@ angular.module('pizzeria', [
             .when('/panier', {
                 template: `<panier></panier>`
             })
-
             .when('/login', {
                 template: `<login></login>`
             })
-
             .when('/inscription', {
             template: `<inscription></inscription>`
             })
-
              .when('/pizzas', {
                 template: `<pizza></pizza>`
             })
-
+            .when('/moncompte/:id?',{
+                template: `<moncompte></moncompte>`
+            })
             .otherwise('/')
     });
