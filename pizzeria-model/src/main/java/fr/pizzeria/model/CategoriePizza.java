02daa6b48e3fd03a995e10ea7a1808d011f74c6c
joel T <joel.torib@gmail.com>
2017-02-01 09:33:05 +0100

    rebase kevin pizza.component.js --> suppression modal

--- pizzeria-model/src/main/java/fr/pizzeria/model/CategoriePizza.java
+++ pizzeria-model/src/main/java/fr/pizzeria/model/CategoriePizza.java
@@ -1,18 +1,21 @@
 package fr.pizzeria.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum CategoriePizza {
 	VIANDE("Viande"), POISSON("Poisson"), SANS_VIANDE("Sans Viande");
-	
+
 	private String libelle;
 
 	private CategoriePizza(String libelle) {
 		this.libelle = libelle;
 	}
 
+	@JsonValue
 	public String getLibelle() {
 		return libelle;
 	}
-	
+
 	@Override
 	public String toString() {
 		return libelle;
