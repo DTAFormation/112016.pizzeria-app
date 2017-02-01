95432a19bcafe542315e400289758238d90a8974
joel T <joel.torib@gmail.com>
2017-02-01 09:33:05 +0100

    rebase kevin pizza.component.js --> suppression modal

--- pizzeria-website/src/pizza/pizza.component.js
+++ pizzeria-website/src/pizza/pizza.component.js
@@ -9,14 +9,46 @@ class PizzaController{
         .then(pizzas =>{
             this.pizzas = pizzas
         } );
-       
-    }
 
-  afficherModale(pizza) {
-
-        this.currentPizza = pizza;
+       this.getCateg();
+       this.initPizzas();
 
     }
+
+
+    initPizzas(){
+             this.PizzaService.getPizzas()
+        .then(pizzas => this.pizzas = pizzas );
+           
+       }
+
+       sort(predicat){
+        if(predicat !="Tous"){
+        this.pizzas =   this.Pizzaservice.getPizzas()
+           .then(pizzas => {
+               this.pizzas = pizzas
+             .filter((pizza)=> pizza.categorie == predicat);
+           })
+         }else{
+         this.initPizzas();
+         }
+       }
+
+    getCateg(){
+          this.PizzaService.getPizzas()
+           .then(pizzas =>{
+             let categObj = _.countBy(pizzas, 'categorie');
+             let categObjArr = []
+             Object.getOwnPropertyNames(categObj).forEach(function(val, idx, array) {
+             categObjArr.push({ nom :val,
+             nb : categObj[val]})   
+             });
+             categObjArr.reduce((acc, obj)=>this.nbMax = (acc += obj.nb),0);
+             categObjArr.push({nom : "Tous", nb : this.nbMax});
+             this.categs = categObjArr;
+           } )
+     }
+
 }
 
  export const Pizza = {
