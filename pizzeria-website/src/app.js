57f3a6b0fda2b877de1522bc0d58769cf070af13
kevdev <prepontkevin@gmail.com>
2017-02-02 13:11:52 +0100

    Lister Boissons + merge client USW014 #37

--- pizzeria-website/src/app.js
+++ pizzeria-website/src/app.js
@@ -2,7 +2,6 @@ import angular from 'angular'
 import ngRoute from 'angular-route'
 import ngStorage from 'ngstorage'
 
-<<<<<<< HEAD
 import { Home } from './home/home.component'
 import { Product } from './product/product.component'
 import { ModalPizza } from './modal/modal.component'
