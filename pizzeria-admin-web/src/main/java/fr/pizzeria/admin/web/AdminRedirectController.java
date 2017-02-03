f767cf8da469da49528dbe6ec0aa6369070da1fc
Fawzi NASSIM <fawzi.nassim@gmail.com>
2017-02-03 15:22:56 +0100

    Redirection vers la page d'accueil sur "/" + drop and create dans le persistence.xml

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/AdminRedirectController.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/AdminRedirectController.java
@@ -1,15 +1,13 @@
 package fr.pizzeria.admin.web;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-@WebServlet("/admin")
+@WebServlet({"/admin", "/"})
 public class AdminRedirectController extends HttpServlet{
 
 	/**
