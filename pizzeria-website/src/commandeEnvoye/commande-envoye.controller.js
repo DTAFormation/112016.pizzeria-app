4d573148cc349fb72a4b320c08708ef7acbaed55
Jeffrey Porcheron <nivvdiy@darkadia-studio.com>
2017-02-03 16:54:20 +0100

    #USW010 Commande envoyée

--- pizzeria-website/src/commandeEnvoye/commande-envoye.controller.js
+++ pizzeria-website/src/commandeEnvoye/commande-envoye.controller.js
@@ -2,12 +2,13 @@
 import css from './commande-envoye.component.css';
 
 export default class CommandeEnvoyeController {
-    constructor(CommandeService, $routeParams, UtilService, $scope) {
+    constructor(CommandeService, $routeParams, UtilService, $scope, $interval) {
 
         this.CommandeService = CommandeService;
         this.id = $routeParams.id;
         this.UtilService = UtilService;
         this.$scope = $scope;
+        this.$interval = $interval;
 
     }
 
@@ -20,17 +21,24 @@ export default class CommandeEnvoyeController {
                 this.boissons = commande[0].boissons;
                 this.desserts = commande[0].desserts;
                 this.entrees = commande[0].entrees;
-                setInterval(this.verifyCommandeState(), 1000);
+                this.stop = this.$interval(
+                    (function(self) {
+                        return function() {
+                            self.verifyCommandeState();
+                        }
+                    })(this),5000);
             });
 
     }
 
     verifyCommandeState(){
-            this.CommandeService.getCommandeById(this.id)
+        if(this.statut === "LIVRER"){
+            this.$interval.cancel(this.stop);
+        }
+        this.CommandeService.getCommandeById(this.id)
             .then(commande => {
                 if(this.statut != commande[0].statut){
                     this.statut = commande[0].statut;
-                    this.$scope.apply();
                 }
             });
     }
