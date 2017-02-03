e96d5f3b1b9dcdccb0ce72a8532437e9421afe29
Jeffrey Porcheron <nivvdiy@darkadia-studio.com>
2017-02-03 18:02:12 +0100

    Correction de l'affichage des prix

--- pizzeria-website/src/shared/service/util.service.js
+++ pizzeria-website/src/shared/service/util.service.js
@@ -2,6 +2,6 @@ export class UtilService {
     constructor() {}
 
     getPrix(prix, langage, devise) {
-        return prix.toLocaleString(langage, { style: 'currency', currency: devise })
+        return prix.toLocaleString(langage, { style: 'currency', currency: devise });
     }
 }
