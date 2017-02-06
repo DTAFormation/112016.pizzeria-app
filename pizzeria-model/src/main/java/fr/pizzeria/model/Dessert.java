d67fa40519f9096f8d73dad06cef20a14941191b
cponsine <colineponsinet@gmail.com>
2017-02-06 16:48:05 +0100

    USW016 lister desserts!

--- pizzeria-model/src/main/java/fr/pizzeria/model/Dessert.java
+++ pizzeria-model/src/main/java/fr/pizzeria/model/Dessert.java
@@ -3,6 +3,8 @@ package fr.pizzeria.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,6 +19,8 @@ public class Dessert {
 	private BigDecimal prix;
 	private String urlImage;
 	private String type;
+	@Enumerated(EnumType.STRING)
+	private CategorieDessert categorie;
 
 	public Dessert() {
 
@@ -28,7 +32,7 @@ public class Dessert {
 		this.nom = nom;
 		this.prix = prix;
 		this.urlImage = urlImage;
-		this.type = "dessert";
+		this.setType("dessert");
 	}
 
 	public Integer getId() {
@@ -63,4 +67,19 @@ public class Dessert {
 		this.urlImage = urlImage;
 	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public CategorieDessert getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(CategorieDessert categorie) {
+		this.categorie = categorie;
+	}
 }
--- pizzeria-website/src/shared/service/dessert.service.js
+++ pizzeria-website/src/shared/service/dessert.service.js
@@ -1,31 +1,24 @@
 const api = 'http://localhost:3000/desserts'
 
 export class DessertService {
+    
     constructor($http) {
-
         this.$http = $http;
         this.desserts = this.$http.get(api)
             .then(response => {
-
                 response.data.forEach(function (element) {
                     element.type = "dessert"
                 }, this);
-
                 return response.data;
             }
-
-
-            );
-
+        );
     }
-
 
     getDesserts() {
         return this.desserts;
     }
 
     getdessertsById(id) {
-
         return this.desserts.then(desserts => _.find(desserts, p => p.id === id));
     }
 
