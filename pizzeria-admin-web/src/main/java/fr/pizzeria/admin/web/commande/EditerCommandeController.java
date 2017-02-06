310670fbcd901b7119c1d8737cd4506e229f44f5
joelTo <joel.torib@gmail.com>
2017-02-06 17:33:46 +0100

    #7 - Avant changement radical des jsp

--- pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/commande/EditerCommandeController.java
+++ pizzeria-admin-web/src/main/java/fr/pizzeria/admin/web/commande/EditerCommandeController.java
@@ -2,9 +2,7 @@ package fr.pizzeria.admin.web.commande;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -19,7 +17,6 @@ import fr.pizzeria.admin.metier.ClientService;
 import fr.pizzeria.admin.metier.CommandeService;
 import fr.pizzeria.admin.metier.LivreurService;
 import fr.pizzeria.admin.metier.PizzaService;
-import fr.pizzeria.admin.metier.form.PizzaForm;
 import fr.pizzeria.model.Commande;
 import fr.pizzeria.model.Pizza;
 import fr.pizzeria.model.Statut;
@@ -31,54 +28,56 @@ public class EditerCommandeController extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 2376353383377147562L;
-	private static final String VUE_AJOUTER_COMMANDE 	= "/WEB-INF/views/commandes/editerCommande.jsp";
-	private static final String LIST_CONTROLLER		= "/admin/commandes/list";
+	private static final String VUE_AJOUTER_COMMANDE = "/WEB-INF/views/commandes/editerCommande.jsp";
+	private static final String LIST_CONTROLLER = "/admin/commandes/list";
 
-	@Inject private CommandeService commandeService;
-	@Inject private ClientService clientService;
-	@Inject private LivreurService livreurService;
-	@Inject private PizzaService pizzaService;
-	
+	@Inject
+	private CommandeService commandeService;
+	@Inject
+	private ClientService clientService;
+	@Inject
+	private LivreurService livreurService;
+	@Inject
+	private PizzaService pizzaService;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Integer id = Integer.parseInt(req.getParameter("id"));
 		req.setAttribute("pizzas", pizzaService.findAll());
 		req.setAttribute("clients", clientService.listerClients());
 		req.setAttribute("livreurs", livreurService.findAll());
-		req.setAttribute("statut", commandeService.getListStatuts());
-		
+		req.setAttribute("commande", commandeService.findAll().get(id));
+
 		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher(VUE_AJOUTER_COMMANDE);
-	    dispatcher.forward(req, resp);
-		
+		dispatcher.forward(req, resp);
+
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String idCommande = req.getParameter("id");
 		Commande commande = commandeService.get(Integer.parseInt(idCommande));
-		
+
 		Integer clientId = Integer.parseInt(req.getParameter("clientId"));
 		Integer livreurId = Integer.parseInt(req.getParameter("livreurId"));
 		Statut statut = Statut.valueOf(req.getParameter("statut"));
 		List<Pizza> pizzas = new ArrayList<>();
 		String[] parameterValues = req.getParameterValues("pizzas");
 		BigDecimal total = new BigDecimal(0);
-		for(String id : parameterValues){
+		for (String id : parameterValues) {
 			Pizza pizza = pizzaService.get(Integer.parseInt(id));
 			pizzas.add(pizza);
 			total = total.add(pizza.getPrix());
 		}
-		
+
 		commande.setClientId(clientService.retrieveClient(clientId));
 		commande.setLivreurId(livreurService.get(livreurId));
 		commande.setPizzas(pizzas);
 		commande.setTotal(total);
 		commande.setStatut(statut);
-		
+
 		commandeService.update(commande, Integer.parseInt(idCommande));
-		
-		
 		resp.sendRedirect(req.getContextPath() + LIST_CONTROLLER);
-		
+
 	}
 }
