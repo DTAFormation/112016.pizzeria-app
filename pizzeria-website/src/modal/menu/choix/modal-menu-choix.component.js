b307c7d68ad1bf0e7b0c96dd5b3f5800dca072d1
Jeffrey Porcheron <nivvdiy@darkadia-studio.com>
2017-02-07 17:36:15 +0100

    #USW013 Composer menu

--- pizzeria-website/src/modal/menu/choix/modal-menu-choix.component.js
+++ pizzeria-website/src/modal/menu/choix/modal-menu-choix.component.js
@@ -13,6 +13,10 @@ class ModalMenuChoixController {
         this.affichagePizzas = [];
         this.affichageDesserts = [];
         this.affichageBoissons = [];
+        this.entryIndex = 0;
+        this.pizzaIndex = 0;
+        this.dessertIndex = 0;
+        this.boissonIndex = 0;
     }
 
     $onInit() {
@@ -69,6 +73,95 @@ class ModalMenuChoixController {
                 }
                 break;
         }
+    }
+
+    nextEntry(){
+        if(this.entryIndex < this.entrees.length-1){
+            this.entryIndex++;
+        } else {
+            this.entryIndex = 0;
+        }
+        this.updateEntryAffichage();
+    }
+    previousEntry(){
+        if(this.entryIndex > 0){
+            this.entryIndex--;
+        } else {
+            this.entryIndex = this.entrees.length-1;
+        }
+        this.updateEntryAffichage();
+    }
+    updateEntryAffichage(){
+        this.affichageEntrees[0] = this.entrees[this.entryIndex];
+        this.affichageEntrees[1] = this.entrees[this.entryIndex+1];
+        this.affichageEntrees[2] = this.entrees[this.entryIndex+2];
+    }
+
+    nextPizza(){
+        if(this.pizzaIndex < this.pizzas.length-1){
+            this.pizzaIndex++;
+        } else {
+            this.pizzaIndex = 0;
+        }
+        this.updatePizzaAffichage();
+    }
+    previousPizza(){
+        if(this.pizzaIndex > 0){
+            this.pizzaIndex--;
+        } else {
+            this.pizzaIndex = this.pizzas.length-1;
+        }
+        this.updatePizzaAffichage();
+    }
+    updatePizzaAffichage(){
+        console.log(this.pizzaIndex);
+        this.affichagePizzas[0] = this.pizzas[this.pizzaIndex];
+        this.affichagePizzas[1] = this.pizzas[this.pizzaIndex+1];
+        this.affichagePizzas[2] = this.pizzas[this.pizzaIndex+2];
+    }
+    
+    nextDessert(){
+        if(this.dessertIndex < this.desserts.length-1){
+            this.dessertIndex++;
+        } else {
+            this.dessertIndex = 0;
+        }
+        this.updateDessertAffichage();
+    }
+    previousDessert(){
+        if(this.dessertIndex > 0){
+            this.dessertIndex--;
+        } else {
+            this.dessertIndex = this.desserts.length-1;
+        }
+        this.updateDessertAffichage();
+    }
+    updateDessertAffichage(){
+        this.affichageDesserts[0] = this.desserts[this.dessertIndex];
+        this.affichageDesserts[1] = this.desserts[this.dessertIndex+1];
+        this.affichageDesserts[2] = this.desserts[this.dessertIndex+2];
+    }
+    
+    nextBoisson(){
+        if(this.boissonIndex < this.boissons.length-1){
+            this.boissonIndex++;
+        } else {
+            this.boissonIndex = 0;
+        }
+        this.updateBoissonAffichage();
+    }
+    previousBoisson(){
+        if(this.boissonIndex > 0){
+            this.boissonIndex--;
+        } else {
+            this.boissonIndex = this.boissons.length-1;
+        }
+        this.updateBoissonAffichage();
+    }
+    updateBoissonAffichage(){
+        this.affichageBoissons[0] = this.boissons[this.boissonIndex];
+        this.affichageBoissons[1] = this.boissons[this.boissonIndex+1];
+        this.affichageBoissons[2] = this.boissons[this.boissonIndex+2];
     }
 
 }
