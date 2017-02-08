00c1d6efb11cb459bfa9cf68b6143c9bacf852de
Astrid Hlavacek <astrid-hlavacek@hotmail.fr>
2017-02-08 16:00:15 +0100

    creation compte

--- pizzeria-website/src/inscription/inscription.controller.js
+++ pizzeria-website/src/inscription/inscription.controller.js
@@ -1,25 +1,36 @@
- export default class InscriptionController{
-    constructor(UserService, $location, $rootScope){
+export default class InscriptionController {
+    constructor(UserService, $location, $rootScope) {
         this.UserService = UserService;
         this.$location = $location;
         this.$rootScope = $rootScope;
     }
 
-    saveUser(form, user){
+    saveUser(form, user) {
         console.log(form.motDePasse)
         if (form.$invalid) return;
         this.user = angular.copy(user);
         this.UserService.saveUser(this.user);
         this.authentification(this.user);
     }
-    authentification(user){
-        this.UserService.connectUser(user)
-            .then(user=> {
-                if(!user){console.log('probleme')}else{
-                this.$rootScope.$emit("userAuth", user);
-                this.$location.path('/moncompte/' + user.id);
-                console.log(user);}
-        })
+    authentification(userc) {
+        this.UserService.connectUser(userc)
+            .then(user => {
+                if (!user) {
+                    console.log('probleme', userc)
+                    this.UserService.connectUser(userc)
+                        .then(user => {
+                            if (!user) { console.log('probleme', userc) } else {
+                                this.$rootScope.$emit("userAuth", user);
+                                this.$location.path('/moncompte/' + user.id);
+                                console.log(user);
+                            }
+                        })
+                } else {
+                    this.$rootScope.$emit("userAuth", user);
+                    this.$location.path('/moncompte/' + user.id);
+                    console.log(user);
+                }
+            })
     }
 }
 
